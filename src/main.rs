3a9517d41cfd90f1a0828fbb477d8babdf147031
SituatedFern <themareepevoloutions@gmail.com>
2022-12-09 20:43:22 -0500

    moderate code optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 //imports
 use std::{thread, time};
-use std::str::FromStr;
 
 use terminal_size::{Width, Height, terminal_size};
 
@@ -8,25 +7,21 @@ fn main() {
     loop { //looping the entire code.
         let size = terminal_size(); //getting the terminal size
         if let Some((Width(w), Height(h))) = size {
-            let hello = w.to_string(); //changing to a string
-            let x = i32::from_str(&hello).unwrap(); //changing the string to a 32 bit signed integer
+            let calc = w as i32 - 30; //accounting for the number of lines that the ascii image takes up.
             print!("\x1B[2J\x1B[1;1H"); //clearing the screen
-            let calc = x - 30; //accounting for the number of lines that the ascii image takes up.
             let amogus = "⠀";
-            for mut temp in 1..calc {
+            for temp in 1..calc {
                 let new = amogus.repeat(temp.try_into().unwrap()).to_string();
                 suschar(new);
-                temp += 1; //counting how many times the loop has been run
 
                 //creating a delay and then clearing the screen.
                 let millis = time::Duration::from_millis(50);
                 thread::sleep(millis);
                 print!("\x1B[2J\x1B[1;1H"); //clears the screen and moves cursor to first line
             }
-            for mut temp2 in (1..calc).rev() {
+            for temp2 in (1..calc).rev() {
                 let new = amogus.repeat(temp2.try_into().unwrap()).to_string();
                 suschar(new);
-                temp2 -= 1; //counting how many times the loop has been run
 
                 //creating a delay and then clearing the screen.
                 let millis = time::Duration::from_millis(50);
