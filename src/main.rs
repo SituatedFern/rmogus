c2ed88211acfe23f0bd6ae994825c92b0b0c8d32
SituatedFern <themareepevoloutions@gmail.com>
2022-12-16 10:56:04 -0500

    added function to remove repeated code

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,18 @@ use std::{thread, time};
 
 use terminal_size::{Width, Height, terminal_size};
 
-fn main() {
+fn main() -> ! {
     loop { //looping the entire code.
         let size = terminal_size(); //getting the terminal size
-        if let Some((Width(w), Height(h))) = size {
+        if let Some((Width(w), Height(_h))) = size {
             let calc = w as i32 - 30; //accounting for the number of lines that the ascii image takes up.
             print!("\x1B[2J\x1B[1;1H"); //clearing the screen
             let amogus = "⠀";
-            for temp in 1..calc {
-                let new = amogus.repeat(temp.try_into().unwrap()).to_string();
-                suschar(new);
-
-                //creating a delay and then clearing the screen.
-                let millis = time::Duration::from_millis(50);
-                thread::sleep(millis);
-                print!("\x1B[2J\x1B[1;1H"); //clears the screen and moves cursor to first line
+            for temp in 1..=calc {
+                inloop(amogus, temp);
             }
-            for temp2 in (1..calc).rev() {
-                let new = amogus.repeat(temp2.try_into().unwrap()).to_string();
-                suschar(new);
-
-                //creating a delay and then clearing the screen.
-                let millis = time::Duration::from_millis(50);
-                thread::sleep(millis);
-                print!("\x1B[2J\x1B[1;1H"); //clears the screen and moves cursor to first line
+            for temp2 in (1..=calc).rev() {
+                inloop(amogus, temp2);
             }
         } else {
             println!("Unable to get terminal size");
@@ -54,4 +42,14 @@ fn suschar (new: String) {
     println!("{}{}", new,"⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀");
     println!("{}{}", new,"⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀");
     println!("{}{}", new,"⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+}
+
+fn inloop (char: &str, iter: i32) {
+    let repeated = char.repeat(iter.try_into().unwrap());
+    suschar(repeated);
+
+    //creating a delay and then clearing the screen.
+    let millis = time::Duration::from_millis(50);
+    thread::sleep(millis);
+    print!("\x1B[2J\x1B[1;1H"); //clears the screen and moves cursor to first line
 }
